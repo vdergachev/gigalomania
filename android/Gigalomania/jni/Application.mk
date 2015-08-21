d458333bc9b816730ac866bc3bdb697f47caa936
Mark Harman <mark.harman@ntlworld.com>
2015-08-22 00:59:38 +0100

    Add commented out gnustl_static (still using stlport_static).

--- android/Gigalomania/jni/Application.mk
+++ android/Gigalomania/jni/Application.mk
@@ -1,7 +1,11 @@
 
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+
+# we need support for STL and C++ exceptions:
 APP_STL := stlport_static 
+#APP_STL := gnustl_static
+
 #APP_ABI := armeabi-v7a armeabi x86
 APP_ABI := armeabi-v7a armeabi
 #APP_ABI := armeabi
