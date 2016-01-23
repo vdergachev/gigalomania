edeb46b40713163abaa1abb0ebb64802156815a2
Mark Harman <mark.harman@ntlworld.com>
2016-01-23 16:52:39 +0000

    Hurray, SDL 2.0.4 finally released!

--- common.h
+++ common.h
@@ -1,8 +1,8 @@
 #pragma once
 
 const int majorVersion = 0;
-const int minorVersion = 28;
-const int patchVersion = 1; // usually 0, but sometimes this is used for minor platform-specific fixes where we don't release on all platforms
+const int minorVersion = 29;
+const int patchVersion = 0; // usually 0, but sometimes this is used for minor platform-specific fixes where we don't release on all platforms
 
 extern bool debugwindow;
 
